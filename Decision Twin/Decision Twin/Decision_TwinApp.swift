b2cd11aead3e9a8b8d8840595e75e3b3a9c0022c
zokyzhou <zokyzhou28@gmail.com>
2026-06-21 23:17:15 -0400

    code

---
+++ Decision Twin/Decision Twin/Decision_TwinApp.swift
@@ -0,0 +1,32 @@
+//
+//  Decision_TwinApp.swift
+//  Decision Twin
+//
+//  Created by Zoky Zhou on 6/21/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Decision_TwinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Decision Twin/Decision TwinTests/Decision_TwinTests.swift
@@ -0,0 +1,18 @@
+//
+//  Decision_TwinTests.swift
+//  Decision TwinTests
+//
+//  Created by Zoky Zhou on 6/21/26.
+//
+
+import Testing
+
+struct Decision_TwinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
